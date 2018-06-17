0e81f1b9744ed7115437df300a74211cd5b90814
Ishan Sethi <ishansethi8@gmail.com>
2018-06-17 04:11:28 -0400

    Solar Code

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 
 
 
+
+var i = 0;
+var observerOptions = {
+  childList: true,
+  attributes: true,
+  subtree: true, //Omit or set to false to observe only changes to the parent node.
+  characterData: true 
+}
+
+var observer = new MutationObserver(callback);
+var targetNode = null;
+
+
 //when the page loads, set up the change detection on the correct Node (targetNode).
 window.onload = function () {
 
-	var front_page = document.getElementById('front-page-content').childNodes[2].childNodes[2].childNodes[3].childNodes[0].childNodes[1].childNodes[3];
+	var front_page = document.getElementById('front-page-content').childNodes[2].childNodes[3];
 	console.log(front_page);
-	console.log(front_page.rows.item(2).innerHTML);
+}
+
+
+//The event handle for the change on the targetNode
+function callback(mutationList, observer) {
+	mutationList.forEach((mutation) => {
+		
+		switch(mutation.type) {
+		case 'childList':
+			//this is the type of change we care about...
+			
+			setTimeout(getTeacherName, 3000);
+			break;
+		case 'attributes':
+			break;
+		}
+	});
+}
+
+function getTeacherName(){
+
+	var correctNode = $("#ptifrmtgtframe").contents().find('MTG_INSTR$0');
+	console.log(correctNode);
+	if(correctNode != null){
+
+	}
 }
 
 
